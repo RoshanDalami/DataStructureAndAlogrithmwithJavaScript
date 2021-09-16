e406a34686689cb512cce35235b88d2d70014712
Roshan Dalami <tech.channel.002@gmail.com>
2021-09-16 23:09:17 +0530

    to new

--- Problem Solving Pattern/maxSubarraySum.js
+++ Problem Solving Pattern/maxSubarraySum.js
@@ -4,4 +4,17 @@ function maxSubarraySum(arr,num){
     if(num>arr.length){
         return null
     }
+    for(var  i = 0 ; i < arr.length -num +1 ; i++){
+        for(var j = 0 ; j<num ; j++){
+            tempSum +=  arr[i+j]
+
+        }
+        if(tempSum > maxSum){
+            maxSum = tempSum
+        }
+    }
+    return maxSum
+    
 }
+let arr= [1,2,3,4,5]
+console.log(maxSubarraySum(arr,2))
