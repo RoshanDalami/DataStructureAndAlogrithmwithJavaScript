5dae7f4b000788fad0d73d7aa04952b9b3aca258
Roshan Dalami <tech.channel.002@gmail.com>
2021-10-07 10:04:40 +0545

    TreeStarting

---
+++ Data Structures/Trees/binarySearch.js
@@ -0,0 +1,53 @@
+class Node {
+    constructor(val) {
+        this.val = val ;
+        this.left = null ;
+        this.right = null ;
+
+        
+    }
+}
+class BinarySearchTree{
+    constructor(){
+        this.root = null ;
+    }
+    insert(val){
+        var newNode = new Node(val);
+         
+        if(this.root === null){
+            this.root = newNode ;
+            return this ;
+        }
+        
+        var current = this.root ;
+            while(true)
+            {
+                if(val < current.val){
+                    if(current.left === null ){
+                        current.left = newNode ;
+                        return this ;
+                    }else{
+                        current = current.left ;
+
+                    }
+                }else if(val > current.val){
+                    if(current.right === null){
+                        current.right = newNode ;
+                        return this;
+                    }
+                        current = current.right ;
+                }
+            }
+    }
+}
+
+var tree = new BinarySearchTree()
+tree.insert(10)
+tree.insert(5)
+tree.insert(13)
+tree.insert(11)
+tree.insert(2)
+tree.insert(11)
+tree.insert(7)
+
+console.log(tree)
