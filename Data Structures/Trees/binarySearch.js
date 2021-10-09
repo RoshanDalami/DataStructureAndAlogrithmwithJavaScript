3eea5e680be303e8a05153113fcc902f2db8a891
Roshan Dalami <tech.channel.002@gmail.com>
2021-10-09 09:38:59 +0545

    TreeSearchingAndFindingFinished

--- Data Structures/Trees/binarySearch.js
+++ Data Structures/Trees/binarySearch.js
@@ -62,16 +62,26 @@ class BinarySearchTree{
        return current ;
 
     }
+    BFS(){
+        var data =[] ,queue = [] ,node = this.root ;
+        queue.push(node)
+        while(queue.length){
+           node = queue.shift();
+           data.push(node)
+           if(node.left) queue.push(node.left)
+           if(node.right)queue.push(node.right)
+        }
+        return data ;
+
+    }
 }
 
 var tree = new BinarySearchTree()
 tree.insert(10)
-tree.insert(5)
-tree.insert(13)
-tree.insert(11)
-tree.insert(2)
-tree.insert(11)
-tree.insert(7)
-console.log(tree.find(50)
-)
+tree.insert(6)
+tree.insert(15)
+tree.insert(3)
+tree.insert(8)
+tree.insert(20)
+console.log( tree.BFS())
 console.log(tree)
