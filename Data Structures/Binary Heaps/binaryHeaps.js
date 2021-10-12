70d0876e521c6528e88f372388fa3e9de815cb2d
Roshan Dalami <tech.channel.002@gmail.com>
2021-10-12 22:07:11 +0545

    BinaryHeapsIntro

--- Data Structures/Binary Heaps/binaryHeaps.js
+++ Data Structures/Binary Heaps/binaryHeaps.js
@@ -1,3 +1,39 @@
-class {
-    
+class MaxBinaryHeap {
+    constructor(value){
+        this.value = [] ;    
+    }
+    insert(element){
+        this.value.push(element)
+        this.bubbleUp();
+        
+        
+    }
+    bubbleUp(){
+       let index = this.value.length - 1;
+       const element = this.value[index];
+       while(index > 0)
+       {
+           let parentIndex = Math.floor((index-1)/2) ;
+           let parent = this.value[parentIndex] ;
+           if( element <= parent){
+               break ;
+           }
+            this.value[parentIndex] = element ;
+            this.value[index] = parent ;
+            index = parentIndex
+           
+       }
+
+
+    }
 }
+let heap = new MaxBinaryHeap();
+heap.insert(41);
+heap.insert(39);
+heap.insert(33);
+heap.insert(18);
+heap.insert(27);
+heap.insert(12);
+heap.insert(55);
+
+console.log(heap)
