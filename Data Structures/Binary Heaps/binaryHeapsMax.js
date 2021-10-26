7f5bd8d84a513cfb741ef76ad0759746dec42104
Roshan Dalami <tech.channel.002@gmail.com>
2021-10-26 23:11:23 +0530

    BinaryHeapsRemoveFinish

--- Data Structures/Binary Heaps/binaryHeapsMax.js
+++ Data Structures/Binary Heaps/binaryHeapsMax.js
@@ -26,9 +26,47 @@ class MaxBinaryHeap {
 
 
     }
-    remove(){
+    extractMax(){
+        const max = this.value[0];
+        const end = this.value.pop();
+        if(this.value.length >0){
+            this.value[0] = end ;
+
+        this.sinkDown()
+
+        }
+        
+        return max ;
         
 
+    } 
+    sinkDown(){
+         let index = 0;
+         const length = this.value.length ;
+         const element = this.value[0];
+         while(true){
+             let leftChildIndex = 2*index + 1 ;
+             let rightChildIndex = 2*index + 2 ;
+             let leftChild,rightChild ;
+             let swap ;
+             if(leftChildIndex < length)
+             {
+                 leftChild = this.value[leftChildIndex]
+                 if(leftChild > element){
+                     swap = leftChildIndex
+                 }
+             }
+             if(rightChildIndex < length){
+                 rightChild = this.value[rightChildIndex]
+                 if((swap === null &&rightChild > element) || (swap !== null && rightChild > leftChild)){
+                     swap = rightChildIndex
+                 }
+             }
+             if(swap == null ) break ;
+             this.value[index] = this.value[swap]
+             this.value[swap ] = element
+             index = swap ;
+         }
     }
 }
 let heap = new MaxBinaryHeap();
@@ -39,5 +77,5 @@ heap.insert(18);
 heap.insert(27);
 heap.insert(12);
 heap.insert(55);
-heap.remove()
+heap.extractMax()
 console.log(heap)
