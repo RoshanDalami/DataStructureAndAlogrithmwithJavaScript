3380276f8491d16d46985fbbe82ee097c005e559
Roshan Dalami <tech.channel.002@gmail.com>
2021-11-05 23:53:15 +0530

    Dijktra's Algorithm

--- Data Structures/Graphs/dijkstra algorithm/dijkstra.js
+++ Data Structures/Graphs/dijkstra algorithm/dijkstra.js
@@ -27,6 +27,29 @@ class WeightedGraph{
         this.adjacencyList[vertex1].push({node:vertex2,weight})
         this.adjacencyList[vertex2].push({node:vertex1,weight})
     }
+    dijkstra(start,finish){
+        const nodes = new PriorityQueue();
+        const distances = {};
+        const previous = {};
+        let smallest ;
+        //built up initial state
+        for(let vertex in this.adjacencyList){
+            if(vertex = start){
+                distances[vertex] = 0
+                nodes.enqueue(vertex , 0) ;
+            }else{
+                distances[vertex] = Infinity ;
+                nodes.enqueue(vertex,Infinity)
+            }
+            previous[vertex] = null ;
+        }
+        // as long as there is something to visit
+        while(nodes.values.length){
+            smallest = nodes.dequeue().val;
+            
+        }
+        
+    }
 }
 
 let graph = new WeightedGraph();
