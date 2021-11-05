92e8cc66d8182d24e04fb0cd1e1e3f3db9ec7a01
Roshan Dalami <tech.channel.002@gmail.com>
2021-11-05 19:28:30 +0530

    Dijkstra'sAlgorithme

--- Data Structures/Graphs/graph.js
+++ Data Structures/Graphs/graph.js
@@ -26,13 +26,83 @@ class Graph {
         }
         delete this.adjacencyList[vertex]
     }
+    depthFirst(StartingNode){
+        const result = [];
+        var visitedVertex = {};
+        let adjacencyList = this.adjacencyList
+        function helper(vertex){
+            if(!vertex){
+                return null ;
+            }
+            visitedVertex[vertex] = true ;
+            result.push(vertex)
+            adjacencyList[vertex].forEach(neighour => { 
+                if(!visitedVertex[neighour]){
+                    return helper(neighour)
+                }
+                
+            });
+        }
+        helper(StartingNode)
+        return result
+    }
+    dfsIterative(startingNode){
+        const visited = {}
+        const result = []
+        const stack = [startingNode]
+        let currentVertex ;
+
+        visited[startingNode] = true ;
+        while(stack.length){
+             currentVertex = stack.pop()
+            result.push(currentVertex)
+            this.adjacencyList[currentVertex].forEach(neighour => {
+                if(!visited[neighour]){
+                    visited[neighour] = true ;
+                    stack.push(neighour)
+                }
+            })
+        }
+        return result
+    }
+    bfs(startingNode){
+        const queue = [startingNode];
+        const visited = {};
+        visited[startingNode] = true ;
+        const result = [];
+        let currentVertex ;
+        while(queue.length){
+            currentVertex = queue.shift();
+            result.push(currentVertex);
+            this.adjacencyList[currentVertex].forEach(neighour =>{
+                if(!visited[neighour]){
+                    visited[neighour] = true ;
+                    queue.push(neighour);
+                }
+            });
+        }
+        return result
+
+    }
 }
 let graph = new Graph()
-graph.addVertex('Roshan')
-graph.addVertex('dalami')
-graph.addVertex('sunar')
-graph.addEdge('Roshan','dalami')
-graph.removeVertex('Roshan')
-graph.addEdge('dalami','sunar')
-graph.addVertex('KingCobra')
+graph.addVertex('A')
+graph.addVertex('B')
+graph.addVertex('C')
+graph.addVertex('D')
+graph.addVertex('E')
+graph.addVertex('F')
+graph.addEdge('A','B')
+graph.addEdge('A','C')
+graph.addEdge('B','D')
+graph.addEdge('C','E')
+graph.addEdge('D','E')
+graph.addEdge('D','F')
+graph.addEdge('E','F')
+graph.depthFirst('A')
+graph.dfsIterative('A')
+graph.bfs('A')
+console.log(graph.depthFirst('A'))
+console.log(graph.dfsIterative('A'))
+console.log(graph.bfs('A'))
 console.log(graph)
